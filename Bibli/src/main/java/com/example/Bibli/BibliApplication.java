7f3233ac4c7eb94f0cd29389defad08ce2ee6a73
nblackbu <sitdikovaelvinaa96@gmail.com>
2023-02-27 22:50:12 +0300

    Initial commit

---
+++ Bibli/src/main/java/com/example/Bibli/BibliApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliApplication.class, args);
+	}
+
+}
