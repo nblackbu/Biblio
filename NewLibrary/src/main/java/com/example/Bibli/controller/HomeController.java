c37190185184615d2c8db2c29c8f1d6b961750a5
nblackbu <sitdikovaelvinaa96@gmail.com>
2023-03-19 23:05:25 +0300

    Правка HomeController: изменено название метода public String home

--- NewLibrary/src/main/java/com/example/Bibli/controller/HomeController.java
+++ NewLibrary/src/main/java/com/example/Bibli/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @GetMapping("")
-    public String books(Model model) {
+    public String home(Model model) {
         return "home";
     }
 }
